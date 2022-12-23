008e591ceeed1610712ed649ce6021388f66234b
Леонид Казанцев <anemiaa1986@gmail.com>
2022-12-23 14:58:22 +0300

    rev1.4

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
-LOGOUT_REDIRECT_URL = 'posts:index'
+# LOGOUT_REDIRECT_URL = 'posts:index'
 
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
